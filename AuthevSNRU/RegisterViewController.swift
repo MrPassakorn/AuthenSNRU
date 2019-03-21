f8989f0b3a8ce2be36859c8acae0c846299afbd2
Mrpassakorn <passsakorn.su59@snru.ac.com>
2019-03-21 16:22:36 +0700

    UploadData Success OK

--- AuthevSNRU/RegisterViewController.swift
+++ AuthevSNRU/RegisterViewController.swift
@@ -40,7 +40,7 @@ class RegisterViewController: UIViewController {
         if (name.count == 0)||(user.count == 0)||(password.count == 0) {
             myAlert(titleString: "Have Space", massageString: "Please Fill All Blank")
         }   else{
-            
+            uploadData(name: name, user: user, password: password)
         }
     }//upload Button
     func myAlert(titleString: String,massageString: String) -> Void {
@@ -58,9 +58,25 @@ class RegisterViewController: UIViewController {
         
         let urlString:String = "https://www.androidthai.in.th/snru/addDataPassakron.php?isAdd=true&Name=\(name)&User=\(user)&Password=\(password)"
         
+        let uri = URL(string: urlString)
+        let request = NSMutableURLRequest(url: uri!)
+        let task = URLSession.shared.dataTask(with: request as URLRequest) {data, response, error in
+            
+            
+            if error != nil {
+                print("Error")
+                
+            } else {
+                if let testData = data {
+                    
+                    let canReadData = NSString(data: testData, encoding: String.Encoding.utf8.rawValue)
+                    print("canRead ==>\(String(describing: canReadData))")
+                }
+            } //if1
+        }//end Task
+        task.resume()
         
-        
-        
+
     }
     
     @IBAction func backButton(_ sender: UIBarButtonItem) {
